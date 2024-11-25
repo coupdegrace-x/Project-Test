3540bcbca2af116281882562894c8c6f4409fc14
Leonid Burshtein <LeonidBurshtein@yandex.ru>
2024-11-25 22:58:40 +0300

    review NegativeLogInTest

--- src/test/java/frontendTests/tests/negativeTests/NegativeLogInTest.java
+++ src/test/java/frontendTests/tests/negativeTests/NegativeLogInTest.java
@@ -5,8 +5,7 @@ import frontendTests.tests.BaseTest;
 import frontendTests.utils.ExistingUser;
 import frontendTests.utils.RandomUserData;
 import frontendTests.utils.TestCase;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -15,87 +14,91 @@ import org.testng.annotations.Test;
         path = "frontendTests/testCases/logInCases/NegativeLogInCases.md")
 public class NegativeLogInTest extends BaseTest {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(NegativeLogInTest.class);
     private static final String COMMON_ERROR_MESSAGE = "Login was unsuccessful. " +
             "Please correct the errors and try again";
 
     private LogInPage logInPage;
 
     @BeforeMethod
-    public void setUpLogInPage() {
+    protected void setUpLogInPage() {
         logInPage = new LogInPage(getDriver());
     }
 
-    public void verifyValidationErrors(String commonErrorMessage, String specificErrorMessage) {
+    public void assertVerifyValidationErrors(String specificErrorMessage) {
+        waitUtils
+                .waitForCondition(ExpectedConditions
+                        .visibilityOf(logInPage.getSpecificMessageError()), 10);
+
         Assert.assertTrue(logInPage.getTextCommonValidationError()
-                .contains(commonErrorMessage), "Common validation error message mismatch");
+                .contains(COMMON_ERROR_MESSAGE), "Common validation error message mismatch");
+
         Assert.assertTrue(logInPage.getTextSpecificValidationError()
                 .contains(specificErrorMessage), "Specific validation error message mismatch");
     }
 
     @Test(description = "Unsuccessful user authorization with empty fields")
     public void testLogInEmptyFields() {
-        LOGGER.info("Start negative testLogInEmptyFields");
+        logger.info("Start negative testLogInEmptyFields");
 
         logInPage.openLogInPageChain()
                 .clickLogIn();
 
-        verifyValidationErrors(COMMON_ERROR_MESSAGE, "No customer account found");
+        assertVerifyValidationErrors("No customer account found");
 
-        LOGGER.info("Finish negative testLogInEmptyFields");
+        logger.info("Finish negative testLogInEmptyFields");
     }
 
     @Test(description = "Unsuccessful authorization of an unregistered user")
     public void testLogInWithUnregisteredUser() {
-        LOGGER.info("Start negative testLogInWithUnregisteredUser");
+        logger.info("Start negative testLogInWithUnregisteredUser");
 
         logInPage.openLogInPageChain()
                 .enterEmailChain(RandomUserData.getRandomEmail())
                 .enterPasswordChain(RandomUserData.getRandomPassword())
                 .clickLogIn();
 
-        verifyValidationErrors(COMMON_ERROR_MESSAGE, "No customer account found");
+        assertVerifyValidationErrors("No customer account found");
 
-        LOGGER.info("Finish negative testLogInWithUnregisteredUser");
+        logger.info("Finish negative testLogInWithUnregisteredUser");
     }
 
     @Test(description = "Unsuccessful authorization of a registered user but not with the correct password")
     public void testLogInWithInvalidPasswordForRegisteredUser() {
-        LOGGER.info("Start negative testLogInWithInvalidPasswordForRegisteredUser");
+        logger.info("Start negative testLogInWithInvalidPasswordForRegisteredUser");
 
         logInPage.openLogInPageChain()
                 .enterEmailChain(ExistingUser.getEmailExistingUser())
                 .enterPasswordChain(RandomUserData.getRandomPassword())
                 .clickLogIn();
 
-        verifyValidationErrors(COMMON_ERROR_MESSAGE, "The credentials provided are incorrect");
+        assertVerifyValidationErrors("The credentials provided are incorrect");
 
-        LOGGER.info("Finish negative testLogInWithInvalidPasswordForRegisteredUser");
+        logger.info("Finish negative testLogInWithInvalidPasswordForRegisteredUser");
     }
 
     @Test(description = "Unsuccessful authorization of a registered user but not with the correct password")
     public void testLogInWithEmptyPassFieldForRegisteredUser() {
-        LOGGER.info("Start negative testLogInWithEmptyPassFieldForRegisteredUser");
+        logger.info("Start negative testLogInWithEmptyPassFieldForRegisteredUser");
 
         logInPage.openLogInPageChain()
                 .enterEmailChain(ExistingUser.getEmailExistingUser())
                 .clickLogIn();
 
-        verifyValidationErrors(COMMON_ERROR_MESSAGE, "The credentials provided are incorrect");
+        assertVerifyValidationErrors("The credentials provided are incorrect");
 
-        LOGGER.info("Finish negative testLogInWithEmptyPassFieldForRegisteredUser");
+        logger.info("Finish negative testLogInWithEmptyPassFieldForRegisteredUser");
     }
 
     @Test(description = "Unsuccessful authorization of a registered user but with an empty email")
     public void testLogInWithEmptyEmailForRegisteredUser() {
-        LOGGER.info("Start negative testLogInWithEmptyEmailForRegisteredUser");
+        logger.info("Start negative testLogInWithEmptyEmailForRegisteredUser");
 
         logInPage.openLogInPageChain()
                 .enterPasswordChain(ExistingUser.getPasswordExistingUser())
                 .clickLogIn();
 
-        verifyValidationErrors(COMMON_ERROR_MESSAGE, "No customer account found");
+        assertVerifyValidationErrors("No customer account found");
 
-        LOGGER.info("Finish negative testLogInWithEmptyEmailForRegisteredUser");
+        logger.info("Finish negative testLogInWithEmptyEmailForRegisteredUser");
     }
 }
